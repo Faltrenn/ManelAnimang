d5cf8bbe70a039884fff2790c9a748d34b068ad6
Emanuel Alves <manelzaum@icloud.com>
2024-03-13 12:22:00 -0300

    Initial Commit

---
+++ Manganel&Animes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Manganel&Animes
+//
+//  Created by Emanuel on 13/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Manganel&Animes/Manganel_AnimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Manganel_AnimesApp.swift
+//  Manganel&Animes
+//
+//  Created by Emanuel on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Manganel_AnimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
