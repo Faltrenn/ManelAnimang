afed3cf720757b1bfadb68121e4ed57bd2048275
Emanuel Alves <manelzaum@icloud.com>
2024-03-18 22:10:33 -0300

    feat: add MangaCard #10

--- Animang/Views/MangaCardView.swift
+++ Animang/Views/MangaCardView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct MangaCardView: View {
+    let manga: Manga
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            AsyncImage(url: URL(string: manga.image)) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                } else if phase.error != nil {
+                    Text("Error")
+                } else {
+                    ProgressView()
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    MangaCardView()
+    LazyVGrid(columns: [GridItem(.flexible(minimum: 100)), GridItem(.flexible(minimum: 100))]) {
+        MangaCardView(manga: Manga(name: "", image: "https://img.lermanga.org/R/return-of-the-sss-class-ranker/capa.jpg", chapters: []))
+        MangaCardView(manga: Manga(name: "", image: "https://img.lermanga.org/R/return-of-the-sss-class-ranker/capa.jpg", chapters: []))
+        MangaCardView(manga: Manga(name: "", image: "https://img.lermanga.org/R/return-of-the-sss-class-ranker/capa.jpg", chapters: []))
+    }
 }
