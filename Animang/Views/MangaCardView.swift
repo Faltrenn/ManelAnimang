18f4b46ee72330c714704f573fdb357bed073506
Emanuel Alves <manelzaum@icloud.com>
2024-03-18 22:08:30 -0300

    feat: add MangaCard #10

---
+++ Animang/Views/MangaCardView.swift
@@ -0,0 +1,18 @@
+//
+//  MangaCardView.swift
+//  Animang
+//
+//  Created by Emanuel on 18/03/24.
+//
+
+import SwiftUI
+
+struct MangaCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MangaCardView()
+}
