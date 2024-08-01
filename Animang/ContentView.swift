0226a425c7fb270537b45a2a2454df2f286597d9
Emanuel Alves <manelzaum@icloud.com>
2024-08-01 16:43:42 -0300

    reset project

--- Animang/ContentView.swift
+++ Animang/ContentView.swift
@@ -7,22 +7,35 @@
 
 import SwiftUI
 
+enum Pages: CaseIterable {
+    case manga, anime
+    
+    var title: String {
+        switch self {
+        case .manga:
+            "Manga"
+        case .anime:
+            "Anime"
+        }
+    }
+}
+
 struct ContentView: View {
-    @State var page: TabbedPages = .manga
+    @State var page: Pages = .manga
     
     var body: some View {
         ZStack(alignment: .bottom){
             ZStack {
                 switch page {
                 case .manga:
-                    MangaHomeView()
+                    Text("Manga")
                 case .anime:
                     Text("Anime")
                 }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             HStack {
-                ForEach(TabbedPages.allCases, id: \.self) { p in
+                ForEach(Pages.allCases, id: \.self) { p in
                     Button {
                         page = p
                     } label: {
