2e88a157da6e025c351b7ee90af6b0ea0c9c9506
Emanuel Alves <manelzaum@icloud.com>
2024-08-02 12:59:03 -0300

    fix: possibility to crash

--- Animang/Code/Features/Manga/MangaView.swift
+++ Animang/Code/Features/Manga/MangaView.swift
@@ -40,9 +40,9 @@ struct MangaView: View {
                     if error == nil, let data = data, let html = String(data: data, encoding: .utf8) {
                         do {
                             let parse = try SwiftSoup.parse(html)
-                            let title = try parse.select("div[class=post-title]").first()!.text()
-                            let image = try parse.select("div[class=summary_image] a img").first()!.attr("src")
-                            let description = try parse.select("div[class=manga-excerpt]").first()!.text()
+                            let title = try parse.select("div[class=post-title]").first()?.text() ?? ""
+                            let image = try parse.select("div[class=summary_image] a img").first()?.attr("src") ?? ""
+                            let description = try parse.select("div[class=manga-excerpt]").first()?.text() ?? ""
                             let chapters = try parse.select("li[class=wp-manga-chapter] a").map{ try $0.attr("href") }
                             DispatchQueue.main.async {
                                 mangaTitle = title
