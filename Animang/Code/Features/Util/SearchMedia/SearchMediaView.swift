db2da01a7e3ff60cc31814b98a66603d46c7f5b4
Emanuel Alves <manelzaum@icloud.com>
2024-08-20 14:36:50 -0300

    add: search media function.

--- Animang/Code/Features/Util/SearchMedia/SearchMediaView.swift
+++ Animang/Code/Features/Util/SearchMedia/SearchMediaView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftSoup
 
 struct Media {
     let link: String
@@ -22,12 +23,38 @@ struct SearchMediaView: View {
             HStack {
                 TextField("Pesquise pelo nome", text: $search)
                 Button("Pesquisar") {
-                    
+                    searchMedia(search: search)
                 }
             }
             ScrollView {
-                
+                ForEach(medias, id: \.link) { media in
+                    Text(media.link)
+                }
             }
+        }
+    }
+    
+    func searchMedia(search: String) {
+        medias = []
+        if let url = URL(string: "https://www.aniture-pt.com.br/search?q=\(search.searchFormat)") {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if error == nil, let data = data, let html = String(data: data, encoding: .utf8) {
+                    do {
+                        let parse = try SwiftSoup.parse(html)
+                        let elements = try parse.select("div[class=post bar hentry]")
+                        for element in elements {
+                            let link = try element.select("a").attr("href")
+                            let imageLink = try element.select("img").attr("src")
+                            let title = try element.select("h2[class=post-title entry-title]").text()
+                            DispatchQueue.main.async {
+                                self.medias.append(Media(link: link, title: title, imageLink: imageLink))
+                            }
+                        }
+                    } catch {
+                        print("ERROR: ", error)
+                    }
+                }
+            }.resume()
         }
     }
 }
