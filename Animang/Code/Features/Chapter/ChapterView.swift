e0186db3d86e2993c7e065ace73b8cf9f7fdbd8e
Emanuel Alves <manelzaum@icloud.com>
2024-09-06 16:48:57 -0300

    fix crash and add ordered images view.

--- Animang/Code/Features/Chapter/ChapterView.swift
+++ Animang/Code/Features/Chapter/ChapterView.swift
@@ -31,40 +31,60 @@ struct ChapterView: View {
                                 }
                         } else if phase.error != nil {
                             Color.red
+                                .onAppear {
+                                    print(phase.error)
+                                }
                         } else {
                             ProgressView()
                         }
                     }
                 }
-                ForEach(imageIndex..<images.count, id: \.self) { _ in
-                    ProgressView()
+                if images.count > 0 {
+                    ForEach(imageIndex..<images.count, id: \.self) { _ in
+                        ProgressView()
+                    }
                 }
             }
         }
         .onAppear {
-            if let url = URL(string: chapterLink) {
-                URLSession.shared.dataTask(with: url) { data, response, error in
-                    if let data = data, let html = String(data: data, encoding: .utf8), error == nil {
-                        do {
-                            let parse = try SwiftSoup.parse(html)
-                            let elements = try parse.select(mangaSelector.chapterImages)
-                            let fetchedImages = try elements.map { try $0.attr("src").trimmingCharacters(in: .whitespacesAndNewlines) }
-                            DispatchQueue.main.async {
-                                images = fetchedImages
-                                imageIndex += 1
-                            }
-                        } catch {
-                            print("ERROR: ", error)
-                        }
+            fetch(link: "\(chapterLink)?style=list") { html in
+                do {
+                    let parse = try SwiftSoup.parse(html)
+                    let elements = try parse.select("div[class=page-break] img")
+                    let fetchedImages = try elements.map({ try $0.attr("data-src").replacing("\n", with: "").replacing("http", with: "https") })
+  
+                    DispatchQueue.main.async {
+                        images = fetchedImages
+                        imageIndex += 1
                     }
-                }.resume()
-            } else {
-                print("url invalida")
-            }
+                } catch {
+                    print("ERROR: ", error)
+                }
+            } 
         }
+    }  
+    
+    func fetch(link: String, completion: @escaping (String) -> Void) {
+        if let request = createRequest(link: link) {
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if error == nil, let data = data, let html = String(data: data, encoding: .utf8) {
+                    completion(html)
+                }
+            }.resume()
+        }
+    }
+    
+    func createRequest(link: String) -> URLRequest? {
+        if let url = URL(string: link) {
+            var request = URLRequest(url: url)
+            request.setValue("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36", forHTTPHeaderField: "User-Agent")
+            return request
+        }
+        return nil
     }
 }
 
 #Preview {
-    ChapterView(chapterLink: "https://lermangas.me/manga/solo-leveling/capitulo-139/", mangaSelector: .leitorDeManga)
+    ChapterView(chapterLink: "https://leitordemanga.com/ler-manga/hunter-x-hunter/portugues-pt-br/capitulo-400/p/1", mangaSelector: .leitorDeManga)
 }
+ 
