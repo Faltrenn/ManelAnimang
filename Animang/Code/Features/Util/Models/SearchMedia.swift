07e94c2df43b03c5fa09fab8ef826aca0f94cd7a
Emanuel Alves <manelzaum@icloud.com>
2024-09-09 16:10:07 -0300

    move function to utils models.

--- Animang/Code/Features/Util/Models/SearchMedia.swift
+++ Animang/Code/Features/Util/Models/SearchMedia.swift
@@ -23,3 +23,22 @@ struct Media {
     let link: String
     let imageLink: String
 }
+
+func fetch(link: String, completion: @escaping (String) -> Void) {
+    if let request = createRequest(link: link) {
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if error == nil, let data = data, let html = String(data: data, encoding: .utf8) {
+                completion(html)
+            }
+        }.resume()
+    }
+}
+
+func createRequest(link: String) -> URLRequest? {
+    if let url = URL(string: link) {
+        var request = URLRequest(url: url)
+        request.setValue("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36", forHTTPHeaderField: "User-Agent")
+        return request
+    }
+    return nil
+}
